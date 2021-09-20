7be1e5095710b17421667cfc381a85c408cbd88b
Hannnst <hannanikolines@outlook.com>
2021-09-20 18:12:04 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-export default function App() {
+// Create a euro to dollar (and viceversa) convertor. It needs to take the input from one component and then render the converted value from a child component.
+
+// It is up to you which component does the actual conversion...
+
+// Extra:
+// - Use an external API to get real exchanges rates, you can use this one: https://currencylayer.com/
+// (they will ask you to register and will give you a free plan and an api key)
+
+// > Use js fetch to get the data from the API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+// ```
+// fetch(`http://www.apilayer.net/api/live?access_key=${your_api_key_here}`)
+// .then( res => res.json())
+// .then( response => console.log(response))
+// .catch( error => console.log(error))
+// ```
+// > In case of errors about CORS , check this link : https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en
+
+function App() {
+  const access_key = '81576e1d5392a99806d7bc17e927f7e7';
+  const [data, setData] = useState('');
+  const URL =
+    'https://api.currencylayer.com/convert ? access_key = 81576e1d5392a99806d7bc17e927f7e7 & from = GBP & to = USD & amount = 10';
+
+  /* fetch(URL)
+    .then((response) => response.json())
+    .then((data) => setData(data[0]));*/
+
+  fetch(`http://www.apilayer.net/api/live?access_key=${access_key}`)
+    .then((res) => res.json())
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>hll</p>
+      <div>{data}</div>
     </div>
   );
 }
+export default App;
